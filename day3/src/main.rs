b36c3a5389fed31e78af5610b2fd90e6692d24e1
tom-anders <13141438+tom-anders@users.noreply.github.com>
2023-12-03 10:37:10 +0100

    solve: day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_derive::aoc_main;
 use itertools::Itertools;
 use utils::*;
@@ -28,6 +30,32 @@ fn solve(input: Input) -> impl Into<Solution> {
         .flat_map(|row| row.0)
         .collect_vec();
 
+    let part2 = numbers
+        .clone()
+        .iter()
+        .filter_map(|number| {
+            number
+                .iter()
+                .find_map(|(pos, _)| grid.all_neighbors(pos).find(|p| *grid.get(*p) == '*'))
+                .map(|gear| {
+                    (
+                        gear,
+                        number
+                            .iter()
+                            .fold("".to_string(), |acc, (_, n)| acc + &n.to_string())
+                            .parse::<u64>()
+                            .unwrap(),
+                    )
+                })
+        })
+        .fold(HashMap::<_, Vec<_>>::new(), |mut map, (gear, n)| {
+            map.entry(gear).or_default().push(n);
+            map
+        })
+        .into_iter()
+        .filter_map(|(_, n)| (n.len() == 2).then(|| n[0] * n[1]))
+        .sum_u64();
+
     let part1 = numbers
         .iter()
         .filter(|number| {
@@ -45,5 +73,5 @@ fn solve(input: Input) -> impl Into<Solution> {
         })
         .sum_u64();
 
-    (part1)
+    (part1, part2)
 }
